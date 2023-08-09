6b30e89d7ee1dea80822e92d04994a0a174d739b
Valerai Dementyeva <lera.dementjeva@yandex.ru>
2023-08-09 16:25:45 +0300

    Initial Commit

---
+++ RightOnTarget/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RightOnTarget
+//
+//  Created by Валерия Дементьева on 09.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
